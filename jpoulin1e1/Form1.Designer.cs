5e4c8bc8e080bb0a82c865765c3109c133e54b56
Jesse <vynlore@hotmail.com>
2019-09-11 13:07:57 -0500

    Completed Ex1e

---
+++ jpoulin1e1/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace jpoulin1e1
+{
+    partial class frmAvgTestScore
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtScoreOne = new System.Windows.Forms.TextBox();
+            this.txtScoreTwo = new System.Windows.Forms.TextBox();
+            this.txtScoreThree = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAverage = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtScoreOne
+            // 
+            this.txtScoreOne.Location = new System.Drawing.Point(142, 53);
+            this.txtScoreOne.Name = "txtScoreOne";
+            this.txtScoreOne.Size = new System.Drawing.Size(77, 31);
+            this.txtScoreOne.TabIndex = 3;
+            this.txtScoreOne.Text = "0";
+            this.txtScoreOne.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtScoreTwo
+            // 
+            this.txtScoreTwo.Location = new System.Drawing.Point(142, 113);
+            this.txtScoreTwo.Name = "txtScoreTwo";
+            this.txtScoreTwo.Size = new System.Drawing.Size(77, 31);
+            this.txtScoreTwo.TabIndex = 4;
+            this.txtScoreTwo.Text = "0";
+            this.txtScoreTwo.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtScoreThree
+            // 
+            this.txtScoreThree.Location = new System.Drawing.Point(142, 173);
+            this.txtScoreThree.Name = "txtScoreThree";
+            this.txtScoreThree.Size = new System.Drawing.Size(77, 31);
+            this.txtScoreThree.TabIndex = 5;
+            this.txtScoreThree.Text = "0";
+            this.txtScoreThree.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Test 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Test 2:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Test 3:";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(300, 47);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(130, 47);
+            this.btnCalculate.TabIndex = 6;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(300, 105);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(130, 47);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "C&lear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(300, 165);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(130, 47);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 255);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Average:";
+            // 
+            // txtAverage
+            // 
+            this.txtAverage.Location = new System.Drawing.Point(142, 252);
+            this.txtAverage.Name = "txtAverage";
+            this.txtAverage.ReadOnly = true;
+            this.txtAverage.Size = new System.Drawing.Size(77, 31);
+            this.txtAverage.TabIndex = 10;
+            this.txtAverage.TabStop = false;
+            // 
+            // frmAvgTestScore
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 362);
+            this.Controls.Add(this.txtAverage);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtScoreThree);
+            this.Controls.Add(this.txtScoreTwo);
+            this.Controls.Add(this.txtScoreOne);
+            this.Name = "frmAvgTestScore";
+            this.Text = "jpoulin1e1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtScoreOne;
+        private System.Windows.Forms.TextBox txtScoreTwo;
+        private System.Windows.Forms.TextBox txtScoreThree;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAverage;
+    }
+}
+
---
+++ jpoulin1e1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace jpoulin1e1
+{
+    public partial class frmAvgTestScore : Form
+    {
+        public frmAvgTestScore()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            txtAverage.Text = ((Convert.ToDecimal(txtScoreOne.Text)
+                + Convert.ToDecimal(txtScoreTwo.Text)
+                + Convert.ToDecimal(txtScoreThree.Text))
+                / 3m)
+                .ToString("0.00");
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            txtScoreOne.Text = "0";
+            txtScoreTwo.Text = "0";
+            txtScoreThree.Text = "0";
+            txtAverage.Text = "";
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
