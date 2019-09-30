f175cf8156d79fb840ee6af9aff99ef9b19ed415
Jesse <vynlore@hotmail.com>
2019-09-29 21:33:51 -0500

    Completed Ex2a

---
+++ jpoulin2a1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace jpoulin2a1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            //Declare variables and calculate
+            decimal subTotal = Convert.ToDecimal(txtSubtotal.Text);
+            decimal discountPercent = Convert.ToDecimal(txtDiscountPercent.Text) / 100;
+            txtDiscountAmount.Text = (subTotal * discountPercent).ToString("0.00");
+            decimal discountAmount = Convert.ToDecimal(txtDiscountAmount.Text);
+            txtTotal.Text = (subTotal - discountAmount).ToString("0.00");
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
