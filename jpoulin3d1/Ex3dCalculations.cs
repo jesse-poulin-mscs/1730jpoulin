749e42f0f1d52ccf67c523a42d8b3ad71dfa82a8
Jesse <vynlore@hotmail.com>
2019-12-15 19:07:57 -0600

    Completed Ex3D

---
+++ jpoulin3d1/Ex3dCalculations.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jpoulin3d1
+{
+    public class Ex3dCalculations
+    {
+        public static string ArrayToString(int[] values)
+        {
+            // Create StringBuilder object: sbResult
+            // loop
+            //     convert current value to string, append to sbResult
+            //     append ", " to sbResult
+            // convert sbResult to string: result
+            // remove last ", " from result
+            // return result
+            StringBuilder sbResult = new StringBuilder(200);
+            string result;
+            foreach (int value in values)
+            {
+                sbResult.Append(value);
+                sbResult.Append(", ");
+            }
+            int index = sbResult.ToString().LastIndexOf(", ");
+            result = sbResult.ToString();
+            result = result.Remove(index, 2);
+            return result;
+        }
+
+        public static int ValueCount(int[] values, int searchValue)
+        {
+            // create counter variable
+            // loop
+            //     increment counter if current value equals searchValue
+            // return counter
+            int count = 0;
+            foreach (int value in values)
+            {
+                int currentValue = value;
+                if (currentValue == searchValue)
+                    count += 1;
+            }
+            return count;
+        }
+
+        public static int RangeCount(int[] values, int searchMin, int searchMax)
+        {
+            // create counter variable
+            // loop
+            //     increment counter if current value between min and max (inclusive)
+            // return counter
+            int count = 0;
+            foreach (int value in values)
+            {
+                int currentValue = value;
+                if (currentValue >= searchMin && currentValue <= searchMax)
+                    count += 1;
+            }
+            return count;
+        }
+    }
+}
---
+++ jpoulin3d1UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("jpoulin3d1UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("jpoulin3d1UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("eb25056c-be56-4e0a-9f9b-b38d9b76a5f8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
